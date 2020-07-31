c61e7ccadd8a3d9d2b6184f3df474f037eb79541
Traphume <traphumedev@gmail.com>
2020-07-31 23:36:41 +0700

    Added ping to client for server

--- stdlib/echo_ping/server/main.go
+++ stdlib/echo_ping/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/net/websocket"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,12 +17,27 @@ func main() {
 	http.Handle("/echo-ping", server)
 
 	// Start the server
-	log.Fatal(http.ListenAndServe("0.0.0.0:7777", nil))
+	log.Fatal(http.ListenAndServe("localhost:7777", nil))
 }
 
-// Our websocket handler will echo any client message  and randomly pings the client by itself
 func Handler(ws *websocket.Conn) {
+	defer ws.Close()
+
+	// Sends message periodically
+	go func() {
+		i := 1
+		for {
+			if _, err := ws.Write([]byte(fmt.Sprintf("[FROM SERVER] %d time!", i))); err != nil {
+				log.Println("Connection Closed by client")
+			}
+
+			time.Sleep(time.Second * 10)
+			i++
+		}
+	}()
+
+	//Read message infinitely
 	if _, err := io.Copy(ws, ws); err != nil {
-		ws.Close()
+		return
 	}
 }
